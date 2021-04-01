15798f0899ad8dc285368332e71a8ad9d34cf971
Rafael Pazos <rarod@microsoft.com>
2021-04-01 15:00:16 +0200

    fixed some dependencies to gateway

--- spoke2.tf
+++ spoke2.tf
@@ -43,7 +43,7 @@ resource "azurerm_virtual_network_peering" "spoke2-hub-peer" {
   allow_virtual_network_access = true
   allow_forwarded_traffic = true
   allow_gateway_transit   = false
-  use_remote_gateways     = true
+  use_remote_gateways     = false
   depends_on = [azurerm_virtual_network.spoke2-vnet, azurerm_virtual_network.hub-vnet]
 }
 
